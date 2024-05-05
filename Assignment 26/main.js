b2e0d514bd4a10a6cb56847f6efe7db2e0490c05
ali hasan <alihassanraja06@gmail.com>
2024-05-05 11:34:37 +0500

    completed

--- Assignment 26/main.js
+++ Assignment 26/main.js
@@ -1,44 +1,13 @@
-
 //Q26  Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
 // • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
-
 // • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
-
 // • Write one version of this program that runs the if block and another that runs the else block.
-
-
 // Alien's color define kiya he.
-let alienColour :string = 'green';
-
+var alienColour = 'green';
 //  Green color check kiya he.
 if (alienColour === 'green') {
-    
     console.log("Player just earned 5 Rupee!");
 }
-else{
-console.log('the player just earned 10 points.');
-
+else {
+    console.log('the player just earned 10 points.');
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
